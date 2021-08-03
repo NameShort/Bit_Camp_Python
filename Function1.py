09b89603b6c327be01731cb6cf661a8a42ae6a43
NameShort <74339420+NameShort@users.noreply.github.com>
2021-08-03 15:57:12 +0900

    2021.08.03

---
+++ Function1.py
@@ -0,0 +1,17 @@
+#리턴을 하지 않는 함수
+
+def setValue(newValue):
+    #지역변수
+    x=newValue
+    print("지역변수:",x)
+
+#호출
+result =setValue(5)
+print(result)
+
+def swap(x, y):
+    return x, y
+
+#호출
+result = swap(3, 4)
+print(result)
